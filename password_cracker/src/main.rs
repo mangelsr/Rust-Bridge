fc5361f40a8c18ba95c3df341ceaa973c55229c8
Miguel Sanchez <mangelsr25@gmail.com>
2025-08-18 22:09:56 -0500

    [UPD] Password craker to make it more effiecient by reusing string buffer and parallelizing the program

--- password_cracker/src/main.rs
+++ password_cracker/src/main.rs
@@ -2,52 +2,65 @@ use md5::{Digest, Md5};
 use std::{
     fs::File,
     io::{BufRead, BufReader},
+    sync::atomic::{AtomicBool, Ordering},
 };
+
+const NUM_CORES: usize = 8;
+static FOUND: AtomicBool = AtomicBool::new(false);
 
 fn main() {
     let word_list_path = std::env::args().nth(1).unwrap();
     let target_hash = std::env::args().nth(2).unwrap();
 
-    let wordlist = File::open(word_list_path);
-    match wordlist {
-        Ok(file) => {
-            use_wordlist(file, target_hash);
+    std::thread::scope(|s| {
+        for i in 0..NUM_CORES {
+            let word_list_path = word_list_path.clone();
+            let target_hash = target_hash.clone();
+
+            s.spawn(move || {
+                let wordlist = File::open(&word_list_path).unwrap();
+                use_wordlist(&wordlist, &target_hash, i);
+            });
         }
-        Err(e) => {
-            eprintln!("{e}"); // eprint => send to stderr
-            return;
-        }
-    }
+    });
 }
 
-fn use_wordlist(wordlist_file: File, target_hash: String) {
-    // todo!(); // Usefull macro to set todos in code
+fn use_wordlist(wordlist_file: &File, target_hash: &str, thread_id: usize) {
     let mut file_buffer = BufReader::new(wordlist_file);
     let hash_bytes = hex::decode(target_hash).unwrap();
-
-    // Memory re-use version
     let mut line = String::new();
+    let mut line_number = 0;
 
     loop {
-        let Ok(bytes_read) = file_buffer.read_line(&mut line) else {
-            line.clear();
-            continue;
-        };
-
-        if bytes_read == 0 {
-            println!("Password not found");
-            return;
-        }
-
-        let mut hasher = Md5::new();
-        hasher.update(&line);
-        let result = hasher.finalize();
-
-        if hash_bytes == result.as_slice() {
-            println!("Password found; is {line}");
+        if FOUND.load(Ordering::Relaxed) {
             return;
         }
 
         line.clear();
+        let bytes_read = match file_buffer.read_line(&mut line) {
+            Ok(n) => n,
+            Err(_) => continue,
+        };
+
+        if bytes_read == 0 {
+            if thread_id == 0 {
+                println!("Password not found");
+            }
+            return;
+        }
+
+        if line_number % NUM_CORES == thread_id {
+            let mut hasher = Md5::new();
+            hasher.update(line.trim_end().as_bytes());
+            let result = hasher.finalize();
+
+            if hash_bytes == result.as_slice() {
+                FOUND.store(true, Ordering::Relaxed);
+                println!("Password found: {}", line.trim_end());
+                return;
+            }
+        }
+
+        line_number += 1;
     }
 }
